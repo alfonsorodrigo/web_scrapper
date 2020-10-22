26ed3566ce5c9f745873fe64ce86abb7b68fc7f5
Alfonso Feria <alfonso.feria@konfio.mx>
2020-10-22 00:18:27 -0500

    Get Articles Links from HomePage

--- main.py
+++ main.py
@@ -3,17 +3,67 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-from settings import SITES
+import re
 
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
+
+from settings import SITES
+from news_page_objects import HomePage, ArticlePage
+
+is_well_formed_url = re.compile(
+    r"^https?://.+/.+$"
+)  # i.e. https://www.somesite.com/something
+is_root_path = re.compile(r"^/.+$")  # i.e. /some-text
 
 logger = logging.getLogger(__name__)
 
 
-def _news_scraper(news_site):
-    host = SITES[news_site]["url"]
+def _news_scraper(news_site_uid):
+    host = SITES[news_site_uid]["url"]
 
     logging.info("Beginning scraper for {}".format(host))
     logging.info("Finding links in homepage...")
+
+    homepage = HomePage(news_site_uid, host)
+
+    articles = []
+
+    for link in homepage.article_links:
+        article = _fetch_article(news_site_uid, host, link)
+
+        if article:
+            logger.info("Article fetched!")
+            articles.append(article)
+            print(article.title)
+            print(article.body)
+
+    print(len(articles))
+
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info("Start fetching article at {}".format(link))
+
+    article = None
+    try:
+        article = ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warn("Error while fetching article!", exc_info=False)
+
+    if article and not article.body:
+        logger.warn("Invalid article. There is no body.")
+        return None
+
+    return article
+
+
+def _build_link(host, link):
+    if is_well_formed_url.match(link):
+        return link
+    elif is_root_path.match(link):
+        return "{host}{uri}".format(host=host, uri=link)
+    else:
+        return "{host}/{uri}".format(host=host, uri=link)
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -1,4 +1,18 @@
 SITES = {
-    "eluniversal": {"url": "http://www.eluniversal.com.mx"},
-    "elpais": {"url": "https://elpais.com"},
+    "eluniversal": {
+        "url": "http://www.eluniversal.com.mx",
+        "queries": {
+            "homepage_article_links": "h4.ce2-Tipo1_Titulo a",
+            "article_body": "div.field-name-body",
+            "article_title": "div.Encabezado-Articulo h1",
+        },
+    },
+    "elpais": {
+        "url": "https://elpais.com",
+        "queries": {
+            "homepage_article_links": "",
+            "article_body": "",
+            "article_title": "",
+        },
+    },
 }
